628816d67982792c46413e55338d8506149d0744
Vi-Linh Nguyen <vilinh27@gmail.com>
2022-01-30 16:08:30 -0600

    coment out unused

--- src/pages/components/Cardtemplate.js
+++ src/pages/components/Cardtemplate.js
@@ -4,14 +4,14 @@ import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
+//import Collapse from "@mui/material/Collapse";
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
+//import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+//import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MoreIcon from "@mui/icons-material/MoreVert";
 import { Link } from "gatsby";
 
